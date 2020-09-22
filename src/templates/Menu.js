aec88a154858b2f50a11c1c90e6e2446b320dfe7
MaiaLucas <barretomaia5466@outlook.com>
2020-09-21 23:25:09 -0300

    adicionando o campo de pesquisa e mudando a props target

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -5,13 +5,17 @@ import throwAway from "../assets/img/throw_away.png";
 import onlineArticle from "../assets/img/online_article.png";
 import amongNature from "../assets/img/among_nature.png";
 import Highlights from "./Highlights";
-import InformationCard from "./InformationCard";
+import InformationCard from "../components/InformationCard";
+import Search from "../components/Search";
 
 export default function Menu() {
   return (
     <div className="Menu">
       <div className="container">
-        <div className=" row justify-content-center">
+        <div className="d-flex justify-content-center my-4">
+          <Search />
+        </div>
+        <div className="d-flex justify-content-center">
           <img src={lotus} className="img-fluid col-sm-6" alt="" />
           <img src={currentLocation} className="img-fluid col-sm-6" alt="" />
         </div>
@@ -26,20 +30,20 @@ export default function Menu() {
               img={onlineArticle}
               headline="Compras Online"
               subheadline="Aproveite para fazer aquelas Ecocomprinhas"
-              target={"/place/3"}
+              target={"/place/3/list"}
             />
             <Highlights
               img={throwAway}
               headline="Ache Pontos de Coleta"
               subheadline="Encontre o posto de coleta perfeito para o seu lixo"
-              target={"/place/1"}
+              target={"/place/1/list"}
             />
             <Highlights
               img={amongNature}
               headline="Encontre as Melhores Experiências"
               subheadline="Aproveite para dar aquela relaxada com as melhores 
               Ecoaventuras da sua região"
-              target={"/place/2"}
+              target={"/place/2/list"}
             />
           </div>
         </div>
