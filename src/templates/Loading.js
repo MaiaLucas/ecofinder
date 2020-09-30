4b5c88caee061d6be0f2056458969014c3f74e8a
MaiaLucas <barretomaia5466@outlook.com>
2020-09-30 15:46:46 -0300

    criação do componente de loading

---
+++ src/templates/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default (params) => {
+  return (
+    <div className="Menu">
+      <div className="container d-flex justify-content-center p-4">
+        <div className="spinner-border text-success" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
+};
