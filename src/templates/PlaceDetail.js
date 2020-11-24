c4cc035edb8bd8cc6a508b02a360a7aa446091e4
MaiaLucas <barretomaia5466@outlook.com>
2020-11-23 22:07:00 -0300

    ajuste no layout dda pagina de detalhe

--- src/templates/PlaceDetail.js
+++ src/templates/PlaceDetail.js
@@ -7,6 +7,8 @@ import { Grid, makeStyles, Typography } from "@material-ui/core";
 import { FiClock, FiEdit, FiPhoneCall } from "react-icons/fi";
 import clsx from "clsx";
 import AuthService from "../services/auth.service";
+import Sidebar from "./SideBar";
+import "../styles/pages/place-detail.css";
 
 const tutorialSteps = [
 	{
@@ -42,13 +44,13 @@ const tutorialSteps = [
 ];
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		display: "flex",
-		flexWrap: "wrap",
-		justifyContent: "space-around",
-		overflow: "hidden",
-		backgroundColor: theme.palette.background.paper,
-	},
+	// root: {
+	// 	display: "flex",
+	// 	flexWrap: "wrap",
+	// 	justifyContent: "space-around",
+	// 	overflow: "hidden",
+	// 	backgroundColor: theme.palette.background.paper,
+	// },
 	paper: {
 		padding: theme.spacing(2),
 		textAlign: "center",
@@ -111,67 +113,72 @@ export default () => {
 
 	if (isLoaded) {
 		return (
-			<Grid container className={clsx(classes.root, "place-details")}>
-				<img
-					className={classes.img}
-					src={arrImages[activeStep].imgPath || amongNature}
-					alt=""
-				/>
+			<div id="place-detail">
+				<Sidebar />
+				<main>
+					<Grid container className="place-details">
+						<img
+							className={classes.img}
+							src={arrImages[activeStep].imgPath || amongNature}
+							alt=""
+						/>
 
-				<Grid item xs={6} className="images">
-					{arrImages.map((image, index) => {
-						return (
-							<button
-								key={image.imgPath}
-								className={activeStep === index ? "active" : ""}
-								type="button"
-								onClick={() => {
-									setActiveStep(index);
-								}}
-							>
-								<img src={image.imgPath} alt={image.label} />
-							</button>
-						);
-					})}
-				</Grid>
-				<Grid item xs={10} className="place-details-content">
-					<h1>
-						{objPlace.title}
-						{objPlace.author == user ? (
-							<Link className={classes.edit} to={"/place/" + id}>
-								<FiEdit size={32} color="#15B6D6" />
-							</Link>
-						) : (
-							""
-						)}
-					</h1>
-					<p>{objPlace.description}</p>
+						<Grid item xs={6} className="images">
+							{arrImages.map((image, index) => {
+								return (
+									<button
+										key={image.imgPath}
+										className={activeStep === index ? "active" : ""}
+										type="button"
+										onClick={() => {
+											setActiveStep(index);
+										}}
+									>
+										<img src={image.imgPath} alt={image.label} />
+									</button>
+								);
+							})}
+						</Grid>
+						<Grid item xs={10} className="place-details-content">
+							<h1>
+								{objPlace.title}
+								{objPlace.author == user ? (
+									<Link className={classes.edit} to={"/place/" + id}>
+										<FiEdit size={32} color="#15B6D6" />
+									</Link>
+								) : (
+									""
+								)}
+							</h1>
+							<p>{objPlace.description}</p>
 
-					<hr />
-					<h2>Endereço</h2>
-					<p>
-						{objPlace.address}, {objPlace.city} - {objPlace.state}
-					</p>
+							<hr />
+							<h2>Endereço</h2>
+							<p>
+								{objPlace.address}, {objPlace.city} - {objPlace.state}
+							</p>
 
-					<hr />
-					<h2>Informações</h2>
-					<div className="open-details">
-						<div className="hour">
-							<FiClock size={32} color="#15B6D6" />
-							{objPlace.opening_days}
-							<br />
-							<Typography variant={"h4"}>
-								Das {objPlace.hr_init} até {objPlace.hr_final}
-							</Typography>
-						</div>
-						<div className="open-on-weekends">
-							<FiPhoneCall size={32} color="#39CC83" />
-							Telefone para contato: <br />
-							<Typography variant={"h4"}>{objPlace.phone}</Typography>
-						</div>
-					</div>
-				</Grid>
-			</Grid>
+							<hr />
+							<h2>Informações</h2>
+							<div className="open-details">
+								<div className="hour">
+									<FiClock size={32} color="#15B6D6" />
+									{objPlace.opening_days}
+									<br />
+									<Typography variant={"h4"}>
+										Das {objPlace.hr_init} até {objPlace.hr_final}
+									</Typography>
+								</div>
+								<div className="open-on-weekends">
+									<FiPhoneCall size={32} color="#39CC83" />
+									Telefone para contato: <br />
+									<Typography variant={"h4"}>{objPlace.phone}</Typography>
+								</div>
+							</div>
+						</Grid>
+					</Grid>
+				</main>
+			</div>
 		);
 	} else if (!isLoaded) {
 		return <Loading />;
