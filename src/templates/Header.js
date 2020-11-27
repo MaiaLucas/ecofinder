44532444a89b26748e4e685216199ea2792bc943
MaiaLucas <barretomaia5466@outlook.com>
2020-11-27 19:22:20 -0300

    ajuste na responsividade

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -15,144 +15,145 @@ import MenuList from "@material-ui/core/MenuList";
 import logo from "../assets/logo.svg";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	appbar: {
-		background: "#f9f9f900",
-		color: "#f9f9f9",
-		boxShadow: "0px 5px 5px 0px rgba(0, 0, 0, 0.01)",
-	},
-	title: {
-		flexGrow: 1,
-		color: "#37C77F",
-		"& img": {
-			width: "120px",
-			// fontSize: "20px",
-		},
-	},
-	paper: {
-		marginRight: theme.spacing(2),
-	},
+  root: {
+    flexGrow: 1,
+  },
+  appbar: {
+    background: "#22222299",
+    height: "55px",
+    color: "#f9f9f9",
+    boxShadow: "0px 5px 5px 0px rgba(0, 0, 0, 0.01)",
+  },
+  title: {
+    flexGrow: 1,
+    color: "#37C77F",
+    "& img": {
+      width: "120px",
+      // fontSize: "20px",
+    },
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
 }));
 export default (props) => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	const { push } = useHistory();
-	const [user, setUser] = useState();
-	const [hasUser, setHasUser] = useState(false);
-	const [open, setOpen] = useState(false);
-	const anchorRef = useRef(null);
+  const { push } = useHistory();
+  const [user, setUser] = useState();
+  const [hasUser, setHasUser] = useState(false);
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
 
-	useEffect(() => {
-		(async () => {
-			await AuthService.getCurrentUser().then((req, res) => {
-				if (req.data) {
-					const { username } = AuthService.userInfo();
-					setUser(username);
-				}
-				setHasUser(req.data);
-			});
-		})();
-		return () => {};
-	}, []);
+  useEffect(() => {
+    (async () => {
+      await AuthService.getCurrentUser().then((req, res) => {
+        if (req.data) {
+          const { username } = AuthService.userInfo();
+          setUser(username);
+        }
+        setHasUser(req.data);
+      });
+    })();
+    return () => {};
+  }, []);
 
-	const logout = () => {
-		AuthService.logout();
-		window.location.reload();
-	};
+  const logout = () => {
+    AuthService.logout();
+    window.location.reload();
+  };
 
-	const login = (e) => {
-		push("/login");
-	};
-	const handleToggle = () => {
-		setOpen((prevOpen) => !prevOpen);
-	};
+  const login = (e) => {
+    push("/login");
+  };
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
-	const handleClose = (event) => {
-		if (anchorRef.current && anchorRef.current.contains(event.target)) {
-			return;
-		}
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
 
-		setOpen(false);
-	};
+    setOpen(false);
+  };
 
-	function handleListKeyDown(event) {
-		if (event.key === "Tab") {
-			event.preventDefault();
-			setOpen(false);
-		}
-	}
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
 
-	// return focus to the button when we transitioned from !open -> open
-	const prevOpen = useRef(open);
-	useEffect(() => {
-		if (prevOpen.current === true && open === false) {
-			anchorRef.current.focus();
-		}
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
 
-		prevOpen.current = open;
-	}, [open]);
+    prevOpen.current = open;
+  }, [open]);
 
-	return (
-		<AppBar position="absolute" className={classes.appbar}>
-			<Toolbar>
-				<Typography variant="h6" className={classes.title}>
-					<img src={logo} alt="logo" onClick={() => push("/")} />
-				</Typography>
-				{!hasUser ? (
-					<Button color="inherit" onClick={(e) => login(e)}>
-						Login
-					</Button>
-				) : (
-					<>
-						<IconButton
-							edge="start"
-							component="span"
-							className="menu-button"
-							color="inherit"
-							aria-label="menu"
-							ref={anchorRef}
-							size="medium"
-							aria-controls={open ? "menu-list-grow" : undefined}
-							aria-haspopup="true"
-							onClick={handleToggle}
-						>
-							<div className="username">Olá, {user}</div>
-							<MenuIcon size="medium" />
-						</IconButton>
-						<Popper
-							open={open}
-							anchorEl={anchorRef.current}
-							role={undefined}
-							transition
-							disablePortal
-						>
-							{({ TransitionProps, placement }) => (
-								<Grow
-									{...TransitionProps}
-									style={{
-										transformOrigin:
-											placement === "bottom" ? "center top" : "center bottom",
-									}}
-								>
-									<Paper>
-										<ClickAwayListener onClickAway={handleClose}>
-											<MenuList
-												autoFocusItem={open}
-												id="menu-list-grow"
-												onKeyDown={handleListKeyDown}
-											>
-												<MenuItem onClick={logout}>Logout</MenuItem>
-											</MenuList>
-										</ClickAwayListener>
-									</Paper>
-								</Grow>
-							)}
-						</Popper>
-					</>
-				)}
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar position="absolute" className={classes.appbar}>
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          <img src={logo} alt="logo" onClick={() => push("/")} />
+        </Typography>
+        {!hasUser ? (
+          <Button color="inherit" onClick={(e) => login(e)}>
+            Login
+          </Button>
+        ) : (
+          <>
+            <IconButton
+              edge="start"
+              component="span"
+              className="menu-button"
+              color="inherit"
+              aria-label="menu"
+              ref={anchorRef}
+              size="medium"
+              aria-controls={open ? "menu-list-grow" : undefined}
+              aria-haspopup="true"
+              onClick={handleToggle}
+            >
+              <div className="username">Olá, {user}</div>
+              <MenuIcon size="medium" />
+            </IconButton>
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom" ? "center top" : "center bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="menu-list-grow"
+                        onKeyDown={handleListKeyDown}
+                      >
+                        <MenuItem onClick={logout}>Logout</MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "../components/Search";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
@@ -11,87 +10,87 @@ import RegisterButton from "./RegisterButton";
 import Footer from "./Footer";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	search: {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	navTabs: {
-		position: "absolute",
-		alignSelf: "center",
-		justifySelf: "center",
-		top: "10%",
-		border: "none !important",
-		background: "#40916d00",
-		width: "500px",
-	},
-	headline: {
-		fontSize: "100px",
-		fontWeight: "bold",
-		color: "#37C77F",
-	},
+  root: {
+    flexGrow: 1,
+  },
+  search: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navTabs: {
+    position: "absolute",
+    alignSelf: "center",
+    justifySelf: "center",
+    top: "10%",
+    border: "none !important",
+    background: "#40916d00",
+    width: "500px",
+  },
+  headline: {
+    fontSize: "100px",
+    fontWeight: "bold",
+    color: "#37C77F",
+  },
 }));
 
 export default function Menu() {
-	let history = useHistory();
-	const handleClick = (type) => {
-		history.push("/places", { type: type });
-	};
-	const classes = useStyles();
-	const [hasUser, setHasUser] = useState(false);
-	useEffect(() => {
-		(async () => {
-			await AuthService.getCurrentUser().then((req, res) => {
-				setHasUser(req.data);
-			});
-		})();
-		return () => {};
-	}, []);
+  let history = useHistory();
+  const handleClick = (type) => {
+    history.push("/places", { type: type });
+  };
+  const classes = useStyles();
+  const [hasUser, setHasUser] = useState(false);
+  useEffect(() => {
+    (async () => {
+      await AuthService.getCurrentUser().then((req, res) => {
+        setHasUser(req.data);
+      });
+    })();
+    return () => {};
+  }, []);
 
-	return (
-		<div className="principal">
-			<Paper className="menu-nav-tabs">
-				<Grid container>
-					<Grid
-						item
-						xs={4}
-						className="menu-nav-tab"
-						onClick={() => handleClick(1)}
-					>
-						Posto de Coleta
-					</Grid>
-					<Grid
-						item
-						xs={4}
-						className="menu-nav-tab"
-						onClick={() => handleClick(2)}
-					>
-						Experiências
-					</Grid>
-					<Grid
-						item
-						xs={4}
-						className="menu-nav-tab"
-						onClick={() => handleClick(3)}
-					>
-						Lojas
-					</Grid>
-				</Grid>
-			</Paper>
-			<Typography variant={"h1"} className={clsx(classes.headline, "headline")}>
-				<span className="subheadline">
-					Tudo que precisa para uma ida sustentável
-				</span>
-				EcoFinder
-			</Typography>
-			<Grid container className={classes.search}>
-				<Search />
-			</Grid>
-			{hasUser && <RegisterButton />}
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="principal">
+      <div className="menu-nav-tabs">
+        <Grid container>
+          <Grid
+            item
+            xs={4}
+            className="menu-nav-tab"
+            onClick={() => handleClick(1)}
+          >
+            Posto de Coleta
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            className="menu-nav-tab"
+            onClick={() => handleClick(2)}
+          >
+            Experiências
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            className="menu-nav-tab"
+            onClick={() => handleClick(3)}
+          >
+            Lojas
+          </Grid>
+        </Grid>
+      </div>
+      <Typography variant={"h1"} className={clsx(classes.headline, "headline")}>
+        EcoFinder
+        <span className="subheadline">
+          Tudo que precisa para uma ida sustentável
+        </span>
+      </Typography>
+      <Grid container className={classes.search}>
+        <Search />
+      </Grid>
+      {hasUser && <RegisterButton />}
+      <Footer />
+    </div>
+  );
 }
