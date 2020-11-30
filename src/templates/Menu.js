8c6db11bd1d642760a769821747eaa99de49f9f4
MaiaLucas <barretomaia5466@outlook.com>
2020-11-30 18:33:53 -0300

    ajuste visual

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -10,87 +10,87 @@ import RegisterButton from "./RegisterButton";
 import Footer from "./Footer";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  search: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  navTabs: {
-    position: "absolute",
-    alignSelf: "center",
-    justifySelf: "center",
-    top: "10%",
-    border: "none !important",
-    background: "#40916d00",
-    width: "500px",
-  },
-  headline: {
-    fontSize: "100px",
-    fontWeight: "bold",
-    color: "#37C77F",
-  },
+	root: {
+		flexGrow: 1,
+	},
+	search: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	navTabs: {
+		position: "absolute",
+		alignSelf: "center",
+		justifySelf: "center",
+		top: "10%",
+		border: "none !important",
+		background: "#40916d00",
+		width: "500px",
+	},
+	headline: {
+		fontSize: "100px",
+		fontWeight: "bold",
+		color: "#37C77F",
+	},
 }));
 
 export default function Menu() {
-  let history = useHistory();
-  const handleClick = (type) => {
-    history.push("/places", { type: type });
-  };
-  const classes = useStyles();
-  const [hasUser, setHasUser] = useState(false);
-  useEffect(() => {
-    (async () => {
-      await AuthService.getCurrentUser().then((req, res) => {
-        setHasUser(req.data);
-      });
-    })();
-    return () => {};
-  }, []);
+	let history = useHistory();
+	const handleClick = (type) => {
+		history.push("/places", { type: type });
+	};
+	const classes = useStyles();
+	const [hasUser, setHasUser] = useState(false);
+	useEffect(() => {
+		(async () => {
+			await AuthService.getCurrentUser().then((req, res) => {
+				setHasUser(req.data);
+			});
+		})();
+		return () => {};
+	}, []);
 
-  return (
-    <div className="principal">
-      <div className="menu-nav-tabs">
-        <Grid container>
-          <Grid
-            item
-            xs={4}
-            className="menu-nav-tab"
-            onClick={() => handleClick(1)}
-          >
-            Posto de Coleta
-          </Grid>
-          <Grid
-            item
-            xs={4}
-            className="menu-nav-tab"
-            onClick={() => handleClick(2)}
-          >
-            Experiências
-          </Grid>
-          <Grid
-            item
-            xs={4}
-            className="menu-nav-tab"
-            onClick={() => handleClick(3)}
-          >
-            Lojas
-          </Grid>
-        </Grid>
-      </div>
-      <Typography variant={"h1"} className={clsx(classes.headline, "headline")}>
-        EcoFinder
-        <span className="subheadline">
-          Tudo que precisa para uma ida sustentável
-        </span>
-      </Typography>
-      <Grid container className={classes.search}>
-        <Search />
-      </Grid>
-      {hasUser && <RegisterButton />}
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="principal">
+			<div className="menu-nav-tabs">
+				<Grid container>
+					<Grid
+						item
+						xs={4}
+						className="menu-nav-tab"
+						onClick={() => handleClick(1)}
+					>
+						Posto de Coleta
+					</Grid>
+					<Grid
+						item
+						xs={4}
+						className="menu-nav-tab"
+						onClick={() => handleClick(2)}
+					>
+						Experiências
+					</Grid>
+					<Grid
+						item
+						xs={4}
+						className="menu-nav-tab"
+						onClick={() => handleClick(3)}
+					>
+						Lojas
+					</Grid>
+				</Grid>
+			</div>
+			<Typography variant={"h1"} className={clsx(classes.headline, "headline")}>
+				EcoFinder
+				<span className="subheadline">
+					Tudo que precisa para uma vida sustentável
+				</span>
+			</Typography>
+			<Grid container className={classes.search}>
+				<Search />
+			</Grid>
+			{hasUser && <RegisterButton />}
+			<Footer />
+		</div>
+	);
 }
